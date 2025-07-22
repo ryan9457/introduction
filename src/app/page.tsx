4a7c45f55137df82e35315bfbafe1c03553a4fdd
Ryan <>
2025-07-22 23:42:35 +0800

    feat: generate static params and routing metadata

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,6 @@
-import Portfolio from "./portfolio/page";
-
 export default function Home() {
   return (
     <main>
-      <Portfolio />
     </main>
   );
 }
---
+++ src/app/portfolio/[gameId]/GameProduct.tsx
@@ -0,0 +1,47 @@
+'use client';
+import React, { useEffect, useRef } from "react";
+
+type Props = {
+  id: number
+  dir: string
+  name: string
+}
+
+export default function GameProduct({ id, dir, name }: Props) {
+    const canvasRef = useRef<HTMLCanvasElement>(null);
+    
+    useEffect(() => {
+      const script = document.createElement("script");
+      script.src = `${dir}/Web.loader.js`; // 根據你的輸出路徑調整
+      script.onload = () => {
+        if (!canvasRef.current) return;
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        (window as any).createUnityInstance(canvasRef.current, {
+          dataUrl: `${dir}/Web.data`,
+          frameworkUrl: `${dir}/Web.framework.js`,
+          codeUrl: `${dir}/Web.wasm`,
+          productName: name || '',
+          productVersion: "1.0",
+        })
+          .then(() => {
+            console.log("Unity Instance loaded.");
+          })
+          .catch((err: Error) => {
+            console.error("failed to load Unity Instance", err);
+          });
+      };
+      document.body.appendChild(script);
+      document.body.style.overflow = 'hidden';
+      return () => {
+        document.body.removeChild(script);
+        document.body.style.overflow = '';
+      };
+    }, []);
+  
+    return (
+      // 橫版最大寬度 = 寬度 × 寬 / 高 ≤ 100vh
+      <div className="w-screen flex items-center justify-center">
+          <canvas ref={canvasRef} id="canvas" className="relative w-full max-w-[66.67vh] aspect-[2/3]" />
+      </div>
+    );
+}
